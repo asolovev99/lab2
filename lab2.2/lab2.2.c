17ae12814fd959be42946c70ad101dfffe141846
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2020-10-31 07:16:47 +0700

    ...

--- lab2.2/lab2.2.c
+++ lab2.2/lab2.2.c
@@ -1,13 +1,22 @@
-long get_fibbonachi_via_recursion(int number);
+#include <stdio.h>
+long long fibbonachi_numbers[91];
+
+long long get_fibbonachi_via_recursion(int number);
 
 int main()
 {
     return 0;
 }
 
-long get_fibbonachi_via_recursion(int number)
+long long get_fibbonachi_via_recursion(int number)
 {
-    if (number == 1)
+    if (number <= 0)
+    {
+        printf("Wrong index");
+
+        return -1;
+    }
+    else if (number == 1)
     {
         return 0;
     }
@@ -15,8 +24,14 @@ long get_fibbonachi_via_recursion(int number)
     {
         return 1;
     }
-    else
+    else if (number > 93)
     {
-        return get_fibbonachi_via_recursion(number - 2) + get_fibbonachi_via_recursion(number - 1);
+        return get_fibbonachi_via_recursion(number - 2) + get_fibbonachi_via_recursion(number - 1); //Переполнение!!!
     }
+    else if (fibbonachi_numbers[number - 3] == 0)
+    {
+        fibbonachi_numbers[number - 3] = get_fibbonachi_via_recursion(number - 2) + get_fibbonachi_via_recursion(number - 1);
+    }
+    
+    return fibbonachi_numbers[number - 3];
 }
